27336e536fee4d7273f8236ca14e6823e5ce484e
Vladislav Yuschuk <glayscorez@gmail.com>
2022-05-16 23:05:11 +0300

    fix: move layout types

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,8 +1,8 @@
-import { Box } from "@chakra-ui/react";
-import { Header } from "./components/Header";
-import type { PropsWithChildren } from "types/utils";
-import type { LayoutProps } from "./components/types";
-import { Footer } from "./components/Footer";
+import {Box} from "@chakra-ui/react";
+import {Header} from "./components/Header";
+import type {PropsWithChildren} from "types/utils";
+import type {LayoutProps} from "./types";
+import {Footer} from "./components/Footer";
 
 export const Layout = ({ children, noFooter = false }: PropsWithChildren<LayoutProps>) => (
   <Box display="flex" flexDirection="column" height="100vh">
